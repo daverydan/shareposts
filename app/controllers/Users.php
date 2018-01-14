a74201b21bc8e8d815b6911598aa16bac86db434
Danny Avery <danny.w.avery@gmail.com>
2018-01-13 22:31:51 -0700

    Start Users Ctrl register() + Register view

---
+++ app/controllers/Users.php
@@ -0,0 +1,32 @@
+<?php 
+
+class Users extends Controller {
+	public function __construct()
+	{
+		
+	}
+
+	public function register()
+	{
+		// Check if POST
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			// process form
+
+		} else {
+			// load form
+			// echo 'load form';
+			$data = [
+				'name' => '',
+				'email' => '',
+				'password' => '',
+				'confirm_password' => '',
+				'name_err' => '',
+				'email_err' => '',
+				'password_err' => '',
+				'confirm_password_err' => '',
+			];
+
+			$this->view('users/register', $data);
+		}
+	}
+}
