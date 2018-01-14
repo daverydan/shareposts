f85c6b0097267cbbef81860d91f78c6f4cd959ec
Danny Avery <danny.w.avery@gmail.com>
2018-01-13 23:06:47 -0700

    Add User Register & Login Views & Methods

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -29,4 +29,24 @@ class Users extends Controller {
 			$this->view('users/register', $data);
 		}
 	}
+
+	public function login()
+	{
+		// Check if POST
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			// process form
+
+		} else {
+			// load form
+			// echo 'load form';
+			$data = [
+				'email' => '',
+				'password' => '',
+				'email_err' => '',
+				'password_err' => '',
+			];
+
+			$this->view('users/login', $data);
+		}
+	}
 }
