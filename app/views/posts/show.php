8f12d3a9302f4cc39a509da44a9aa39feafb00cc
Danny Avery <danny.w.avery@gmail.com>
2018-01-14 20:01:28 -0700

    Edit posts

--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -1,5 +1,6 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
+	<?php flash('post_message'); ?>
 	<a href="<?php echo URLROOT; ?>/posts" class="btn btn-light"><i class="fa fa-backward"></i> Back</a>
 	<br>
 	<h1><?php echo $data['post']->title; ?></h1>
