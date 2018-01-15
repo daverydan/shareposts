42f29daf74b2524f0d42ac890a3cc57b7f460a37
Danny Avery <danny.w.avery@gmail.com>
2018-01-14 18:04:36 -0700

    Post model + display

--- app/models/Post.php
+++ app/models/Post.php
@@ -10,7 +10,17 @@ class Post {
 
 	public function getPosts()
 	{
-		$this->db->query("SELECT * FROM posts");
-		return $this->db->resultSet();
+		$this->db->query('SELECT *,
+											posts.id as postId,
+											users.id as userId,
+											posts.created_at as postCreated,
+											users.created_at as userCreated
+											FROM posts
+											INNER JOIN users
+											ON posts.user_id = users.id
+											ORDER BY posts.created_at DESC
+										');
+		$results = $this->db->resultSet();
+		return $results;
 	}
 }
